815994b1726658892370f5b0d3c1a25dc662636e
unknown <STAGIAIRE@R304-13.demostraining.com>
2016-05-12 12:19:20 +0200

    premier commit : classe Article finie

---
+++ src/com/demos/cine/model/Article.java
@@ -0,0 +1,50 @@
+package com.demos.cine.model;
+
+public class Article {
+	
+	private double prixHT;
+	private String reference;
+	private String designation;
+	private int qtestock;
+	
+	public Article(double prixHT, String reference, String designation, int qtestock) {
+		super();
+		this.prixHT = prixHT;
+		this.reference = reference;
+		this.designation = designation;
+		this.qtestock = qtestock;
+	}
+
+	public double getPrixHT() {
+		return prixHT;
+	}
+	public void setPrixHT(double prixHT) {
+		this.prixHT = prixHT;
+	}
+	public String getReference() {
+		return reference;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public int getQtestock() {
+		return qtestock;
+	}
+	
+	public void incrementStock(int qte) {		
+		qtestock+=qte;		
+	}
+	
+	public void decrementStock(int qte) {		
+		qtestock-=qte;	
+	}
+
+	@Override
+	public String toString() {
+		return "Article [prixHT=" + prixHT + ", reference=" + reference + ", designation=" + designation + ", qtestock="
+				+ qtestock + "]";
+	}
+	
+	
+
+}
